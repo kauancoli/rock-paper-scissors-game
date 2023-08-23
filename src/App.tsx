01fb36c4df9fc684c1b146c2c987358adf67b9a6
Kauan Coli <kauanneves1008@gmail.com>
2023-08-23 20:11:03 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+function App() {
+  return (
+    <S.Main>
+      <S.Title>Rock Paper Scissors</S.Title>
+    </S.Main>
+  );
+}
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100%;
+
+  min-height: 100vh;
+
+  background-color: hsl(214, 47%, 23%) to hsl(237, 49%, 15%);
+`;
+
+export const Title = styled.p`
+  font-size: 1.8rem;
+  font-weight: 800;
+
+  color: hsl(229, 25%, 31%);
+`;
