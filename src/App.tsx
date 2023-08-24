efb0a0a236576ece02162096671fbf377ab1ec09
Kauan Coli <kauanneves1008@gmail.com>
2023-08-23 21:32:10 -0300

    First Steps to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,32 @@ import * as S from './styles';
 function App() {
   return (
     <S.Main>
-      <S.Title>Rock Paper Scissors</S.Title>
+      <S.Header>
+        <img src="/logo.svg" alt="Logo" width={90} height={60} />
+        <S.Score>
+          <p>SCORE</p>
+          <h1>12</h1>
+        </S.Score>
+      </S.Header>
+
+      <S.Icons>
+        <S.Icon>
+          <img src="/icon-paper.svg" alt="Icon Paper" width={40} height={40} />
+        </S.Icon>
+        <S.Icon>
+          <img
+            src="/icon-scissors.svg"
+            alt="Icon Scissors"
+            width={40}
+            height={40}
+          />
+        </S.Icon>
+        <S.Icon>
+          <img src="/icon-rock.svg" alt="Icon Rock" width={40} height={40} />
+        </S.Icon>
+      </S.Icons>
+
+      <S.Button>Rules</S.Button>
     </S.Main>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -6,12 +6,63 @@ export const Main = styled.div`
 
   min-height: 100vh;
 
-  background-color: hsl(214, 47%, 23%) to hsl(237, 49%, 15%);
+  background: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+
+  @media (min-width: 375px) {
+    padding: 3rem;
+  }
 `;
 
-export const Title = styled.p`
-  font-size: 1.8rem;
-  font-weight: 800;
+export const Header = styled.div`
+  @media (min-width: 375px) {
+    border: 0.3rem solid hsl(217, 16%, 45%);
+    border-radius: 0.4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.2rem;
+  }
+`;
 
-  color: hsl(229, 25%, 31%);
+export const Score = styled.div`
+  @media (min-width: 375px) {
+    background-color: white;
+    padding: 0.6rem 2rem;
+    border-radius: 0.8rem;
+
+    p {
+      font-size: 1.2rem;
+      color: hsl(229, 64%, 46%);
+      font-weight: 600;
+      letter-spacing: 0.1rem;
+    }
+
+    h1 {
+      font-size: 4rem;
+      color: hsl(229, 25%, 31%);
+      font-weight: 700;
+    }
+  }
+`;
+
+export const Icons = styled.div`
+  @media (min-width: 375px) {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+  }
+`;
+
+export const Icon = styled.div`
+  @media (min-width: 375px) {
+    background-color: white;
+    border-radius: 100%;
+    padding: 2rem;
+  }
+`;
+
+export const Button = styled.button`
+  @media (min-width: 375px) {
+    font-size: 1.8rem;
+  }
 `;
